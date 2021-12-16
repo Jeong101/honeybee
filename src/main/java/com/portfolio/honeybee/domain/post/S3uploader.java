ab2b12ae0d87304d180b9474caebb997a162038e
yongju6 <jyongju928@gmail.com>
2021-12-16 14:31:51 +0900

    test

--- src/main/java/com/portfolio/honeybee/domain/post/S3uploader.java
+++ src/main/java/com/portfolio/honeybee/domain/post/S3uploader.java
@@ -16,27 +16,44 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.portfolio.honeybee.HoneybeeApplication;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "cloud.aws")
+@Service
 public class S3uploader {
-    @Value("${cloud.aws.s3.bucket}")
-    private static String BUCKET_NAME;
-    @Value("${cloud.aws.credentials.accessKey}")
-    private static String ACCESS_KEY;
-    @Value("${cloud.aws.credentials.secretKey}")
-    private static String SECRET_KEY;
+    // @Value("${cloud.aws.s3.bucket}")
+    // private static String BUCKET_NAME;
+    // @Value("${cloud.aws.credentials.accessKey}")
+    // private static String ACCESS_KEY;
+    // @Value("${cloud.aws.credentials.secretKey}")
+    // private static String SECRET_KEY;
     private AmazonS3 amazonS3;
     private String videoURL;
 
+    @Setter
+    public static class Values {
+        private String accessKey;
+        private String secretKey;
+        private String bucket;
+    }
+
     public S3uploader() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+        Values keys = new Values();
+
+        AWSCredentials awsCredentials = new BasicAWSCredentials(keys.accessKey, keys.secretKey);
         amazonS3 = new AmazonS3Client(awsCredentials);
     }
 
     public void uploadVideos(File video, String title) throws IllegalStateException, IOException {
-
+        Values keys = new Values();
         if (amazonS3 != null) {
             try {
                 // File file = video.transferTo(file);
@@ -44,7 +61,7 @@ public class S3uploader {
                 // + ".mp4";
 
                 PutObjectRequest putObjectRequest = new PutObjectRequest(
-                        BUCKET_NAME /* + "/sub_dir_name" sub directory */, title + ".mp4", video);
+                        keys.bucket /* + "/sub_dir_name" sub directory */, title + ".mp4", video);
                 putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead); // file permission
                 amazonS3.putObject(putObjectRequest); // upload file
                 Thread th = new Thread();
