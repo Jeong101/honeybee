1036c4183a3d36f84238220b999f98e43f4a9031
Jeong101 <chox623@gmail.com>
2021-12-16 15:49:25 +0900

    upload to AWS S3 from temp

--- src/main/java/com/portfolio/honeybee/domain/post/S3uploader.java
+++ src/main/java/com/portfolio/honeybee/domain/post/S3uploader.java
@@ -14,6 +14,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.portfolio.honeybee.HoneybeeApplication;
+import com.portfolio.honeybee.web.VideoController;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -22,42 +23,41 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Configuration
-@ConfigurationProperties(prefix = "aws")
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Service
+@RequiredArgsConstructor
 public class S3uploader {
 
-    private String BUCKET_NAME = "";
-    private String ACCESS_KEY = "";
-    private String SECRET_KEY = "";
+    private String BUCKET_NAME;
+    private String ACCESS_KEY;
+    private String SECRET_KEY;
     private AmazonS3 amazonS3;
-    private String videoURL;
 
     public S3uploader(String BUCKET_NAME, String ACCESS_KEY, String SECRET_KEY) {
         this.BUCKET_NAME = BUCKET_NAME;
         this.ACCESS_KEY = ACCESS_KEY;
         this.SECRET_KEY = SECRET_KEY;
-    }
-
-    public S3uploader() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
         amazonS3 = new AmazonS3Client(awsCredentials);
     }
 
-    public void uploadVideos(File video, String title) throws IllegalStateException, IOException {
+    public void uploadVideos(String savedName, String absolutePath) throws IllegalStateException, IOException {
 
         if (amazonS3 != null) {
             try {
-                // File file = video.transferTo(file);
-                // videoURL = "https://honeybee5sound.s3.ap-northeast-2.amazonaws.com/" + title
-                // + ".mp4";
+
+                File video = new File(absolutePath + savedName);
 
                 PutObjectRequest putObjectRequest = new PutObjectRequest(
-                        BUCKET_NAME /* + "/sub_dir_name" sub directory */, title + ".mp4", video);
+                        BUCKET_NAME /* + "/sub_dir_name" sub directory */, savedName, video);
                 putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead); // file permission
                 amazonS3.putObject(putObjectRequest); // upload file
-                Thread th = new Thread();
-                // UploadVideo uv = new UploadVideo();
-                // uv.testMethod(videoURL);
+
             } catch (AmazonServiceException ase) {
                 ase.printStackTrace();
             } finally {
